557b7f2aa5d47f98ec8a62696ab038544431e094
Bradley <70288773+granitedonut@users.noreply.github.com>
2023-02-06 21:07:39 +0530

    Add files via upload

---
+++ page1.py
@@ -0,0 +1,26 @@
+from tkinter import *
+root=Tk()
+
+def root():
+    root = Tk()
+    root.geometry("800x800") 
+    root.title("Page 1") 
+
+
+def registration():
+    destroy.root()
+    import bradslogin
+    
+Label(text="Welcome", bg="purple", width="30", height="2", font=("Calibri", 13)).pack() 
+Label(text="").pack() 
+
+def login():
+    destroy.root()
+Button(text="Click Me", height="2", width="30").pack() 
+Label(text="").pack() 
+
+
+ 
+root.mainloop() 
+
+
